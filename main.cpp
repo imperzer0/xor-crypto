4e3169a7ca43df4bf29085360c05711068e0b93f
BuildTools <unconfigured@null.spigotmc.org>
2021-04-20 22:09:05 +0300

    added uninstalling completions feature

--- main.cpp
+++ main.cpp
@@ -451,17 +451,17 @@ void completion_init(const char* appname)
 	cmd += appname;
 	cmd += " -e\n";
 	
-	cmd +="complete -c ";
+	cmd += "complete -c ";
 	cmd += appname;
 	cmd += " -f\n";
-	
+
 //	struct stat st{ };
 //	if (!::stat((std::string("/etc/fish/completions/") + appname + ".fish").c_str(), &st))
 //	{
 //		::remove((std::string("/etc/fish/completions/") + appname + ".fish").c_str());
 //	}
 	
-	struct passwd *pw = ::getpwuid(getuid());
+	struct passwd* pw = ::getpwuid(getuid());
 	std::string s(pw->pw_dir);
 	s += "/.config/fish/config.fish";
 	completions = ::fopen(s.c_str(), "wb");
@@ -471,6 +471,145 @@ void completion_init(const char* appname)
 		exit(1);
 	}
 	fwrite(cmd.c_str(), 1, cmd.size(), completions);
+}
+
+class fstream
+{
+public:
+	explicit fstream(FILE* fd) : fd(fd)
+	{ }
+	
+	std::string& getline()
+	{
+		auto* res = new std::string;
+		while (true)
+		{
+			char* tmp[1024]{ };
+			if (!::feof(fd))
+			{
+				size_t bytes_read = 0;
+				if (bytes_read = ::fread(tmp, 1, 1024, fd); bytes_read < 0)
+				{
+					std::cerr << ::strerror(errno) << "\n";
+					return *new std::string;
+				}
+				buffer.append(reinterpret_cast<const char*>(tmp), bytes_read);
+			}
+			
+			int pos = -1;
+			for (int i = 0; i < buffer.size(); ++i)
+			{
+				if (buffer[i] == '\n')
+				{
+					pos = i;
+					break;
+				}
+			}
+			
+			if (pos >= 0)
+			{
+				*res = buffer.substr(0, pos);
+				buffer.erase(buffer.begin(), buffer.begin() + pos + 1);
+				return *res;
+			}
+			
+			if (pos < 0 && !buffer.empty())
+			{
+				*res = buffer;
+				buffer.clear();
+				return *res;
+			}
+		}
+	}
+	
+	void getline(std::string& res)
+	{
+		res.clear();
+		while (true)
+		{
+			char* tmp[1024]{ };
+			if (!::feof(fd))
+			{
+				size_t bytes_read = 0;
+				if (bytes_read = ::fread(tmp, 1, 1024, fd); bytes_read < 0)
+				{
+					std::cerr << ::strerror(errno) << "\n";
+					return;
+				}
+				buffer.append(reinterpret_cast<const char*>(tmp), bytes_read);
+			}
+			
+			int pos = -1;
+			for (int i = 0; i < buffer.size(); ++i)
+			{
+				if (buffer[i] == '\n')
+				{
+					pos = i;
+					break;
+				}
+			}
+			
+			if (pos >= 0)
+			{
+				res = buffer.substr(0, pos);
+				buffer.erase(buffer.begin(), buffer.begin() + pos + 1);
+			}
+			
+			if (pos < 0 && !buffer.empty())
+			{
+				res = buffer;
+				buffer.clear();
+			}
+		}
+	}
+	
+	explicit operator bool() const
+	{
+		return !::feof(fd) || !buffer.empty();
+	}
+
+private:
+	FILE* fd;
+	std::string buffer;
+};
+
+void completion_remove_all_lines_with(const std::string& str)
+{
+	struct passwd* pw = ::getpwuid(getuid());
+	std::string s(pw->pw_dir);
+	s += "/.config/fish/config.fish";
+	auto* read_completions = ::fopen(s.c_str(), "rb");
+	if (read_completions == nullptr)
+	{
+		std::cerr << ::strerror(errno) << "\n";
+		exit(1);
+	}
+	
+	std::string file_content;
+	fstream file(read_completions);
+	while (file)
+	{
+		std::string& line = file.getline();
+		if (line.find(str) == std::string::npos)
+		{
+			file_content += line;
+			file_content += "\n";
+		}
+	}
+	
+	if (completions != nullptr)
+	{
+		::fclose(completions);
+		::remove(s.c_str());
+	}
+	
+	completions = ::fopen(s.c_str(), "wb");
+	if (completions == nullptr)
+	{
+		std::cerr << ::strerror(errno) << "\n";
+		exit(1);
+	}
+	::fwrite(file_content.c_str(), 1, file_content.size(), completions);
 }
 
 int main(int argc, char** argv)
@@ -721,6 +860,12 @@ int main(int argc, char** argv)
 		set_completion(argv[2], "output", new const char* [1]{"(ls -p | grep -v /)"}, 1, "output file");
 		set_completion(argv[2], "passwd", new const char* [0]{ }, 0, "password");
 		set_completion(argv[2], "passwd-file", new const char* [1]{"(ls -p | grep -v /)"}, 1, "file with password");
+	}
+	else if ((action == "uninstall") && argc == 3)
+	{
+		std::string str("complete -c ");
+		str += argv[2];
+		completion_remove_all_lines_with(str);
 	}
 	else
 	{
